f223c3f00e49caff0bfb73d96e83c7b3867458e0
Ana Bela Lima <a.blima@outlook.com>
2022-06-17 19:41:51 +0100

    nav bar done

--- src/components/ Footer/Footer.js
+++ src/components/ Footer/Footer.js
@@ -1,24 +1,43 @@
 // component: 
 
+import './Footer.css';
+
+
 const Footer = () => {
 
     return (
+
+
+ 
         <div id="footerr">
 
-            <footer>
+            <footer id = "footer">
             <nav id="nav">
+
+            
                 <ul>
 
-                    <div id= "li1"><li class="li-nav"> <a href="https://www.addictinggames.com/">Company Info </a> </li></div>
-                    <div id="li2"><li class="li-nav"> <a href= "https://www.addictinggames.com/"> Get Involved </a></li></div>
-                    <div id="li3"><li class="li-nav"> <a href= "https://www.addictinggames.com/">Support  </a></li></div>
-                    <div id= "li4"> <li class="li-nav"> <a href= "">Languages</a></li></div>
-                    <div > <img  id = "cowPic" src= "https://c.tenor.com/XF4caftz0fQAAAAC/cow-farm.gif" alt="Cow gif"></img> </div>
-  
+                    <div id= "li1"> <li className="li-nav"> <a id="anchor1" href="https://www.addictinggames.com/">Company Info </a> </li></div>
+                    <div id="li2"><li className="li-nav"> <a href= "https://www.addictinggames.com/"> Get Involved </a></li></div>
+                    <div id="li3"><li className="li-nav"> <a href= "https://www.addictinggames.com/">Support  </a></li></div>
+                    <div id= "li4"> <li className="li-nav"> <a id="anchor2" href= "">Languages</a></li></div>
+                    <div > <img id="farmLogo" src= "https://hayday.com/graphics/misc/headline_comp.png" alt="Cow gif"></img> </div>
+
+                    {/* <div > <img id = "cowPic" src= "https://c.tenor.com/XF4caftz0fQAAAAC/cow-farm.gif" alt="Cow gif"></img> </div> */}
+                    
+
                </ul>
+              
+
             </nav>
             </footer>
+
+            
         </div>
+
+
+
+        
 
     )
 }
