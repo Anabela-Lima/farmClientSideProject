16517ab8cc549b6bc2711061ea42e63b18c4291c
Ana Bela Lima <a.blima@outlook.com>
2022-06-17 15:45:00 +0100

    first commit footer

---
+++ src/Footer.js
@@ -0,0 +1,111 @@
+// component: 
+
+const Footer = () => {
+
+    return (
+        <div id="Footecd footr">
+
+            <footer>
+            <nav id="nav">
+                <ul>
+
+                    <div id= "li1"><li class="li-nav"> <a href="https://www.addictinggames.com/">Company Info </a> </li></div>
+                    <div id="li2"><li class="li-nav"> <a href= "https://www.addictinggames.com/"> Get Involved </a></li></div>
+                    <div id="li3"><li class="li-nav"> <a href= "https://www.addictinggames.com/">Support  </a></li></div>
+                    <div id= "li4"> <li class="li-nav"> <a href= "">Languages</a></li></div>
+                    
+               </ul>
+            </nav>
+            </footer>
+        </div>
+
+    )
+}
+
+
+/*
+
+ON hover-  drop down 
+
+Company Info- Terms of use , Privacy Policy, Information, Cookies, Cookie Consent
+
+Support-Help, FAQ, Fund our Projects
+
+Languages- A list of languages-  click a lang activate translation feature in browser? 
+
+
+*/
+
+
+export default Footer;
+
+
+
+
+// CSS FOR FOOTER 
+
+
+
+
+/*  */
+
+ 
+// ul {
+//     list-style-type: none;
+//     margin: 0;
+//     padding: 0;
+//     overflow: hidden;
+//     background-color:rgb(153,191,210)
+//   }
+  
+  
+  
+//   li {
+//     float: left;
+//     background-color: rgb(132,177,40);
+  
+//     /* moving the list items to the right */
+//     position: relative;
+//     left:0.2em;
+  
+//   }
+  
+  
+//   /* when hover- hover in black */
+//   li a:hover {
+//     background-color: rgb(35,169,195)}
+  
+//   li a {
+//     display:block;
+//     color: white;
+//     text-align: center;
+//     padding: 16px;
+//     text-decoration:underline;
+//   }
+  
+  
+//   #li1{
+  
+//     position: relative;
+//     left: 10em;
+  
+//   }
+  
+//   #li2{
+  
+//     position: relative;
+//     left: 10em;
+//   }
+  
+//   #li3{
+  
+//     position: relative;
+//     left: 10em;
+//   }
+  
+  
+//   #li4{
+  
+//     position: relative;
+//     left: 10em;
+//   }
