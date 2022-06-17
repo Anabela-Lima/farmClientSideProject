e47813145960fccfea4498076d88c4d7b5851b9e
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-06-17 16:18:28 +0100

    farm container added to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
+import FarmContainer from './containers/FarmContainer';
 
 
 function App() {
   return (
     <>
      <Header />
+     <FarmContainer />
+
     </>
   );
 }
