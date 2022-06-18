15abb8e55ec97295d45b5622ce943134cbb8a875
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-06-18 20:18:15 +0100

    field.js removed?

--- src/components/FieldList.js
+++ src/components/FieldList.js
@@ -1,19 +1,13 @@
-
 import { useEffect } from "react";
 import axios from 'axios'; 
-import Farm from "../Farm/Farm";
-
+import Farm from "./Farm";
 
 const FieldList = ( {fieldList, setFieldList} ) => {
 
-
             useEffect( () => { axios.get('http://127.0.0.1:8080/fields')
-            .then(res => {
-        
-                const fieldList = res.data;
+            .then(res => {const fieldList = res.data;
             setFieldList(fieldList);
-             }).catch(err => console.log(err));
-
+             }).catch(err => console.log(err)); 
   });
 
   return (
