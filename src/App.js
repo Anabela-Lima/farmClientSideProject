83475a1c73c37c14cd2c8fab2373a3214691ae7e
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-06-18 20:54:11 +0100

    sound.js, sound.css readded and linked

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import FarmContainer from './containers/FarmContainer';
+import PlaySound from './components/Sound/sound';
 
 
 function App() {
   return (
     <>
      <Header />
+     <PlaySound />
      <FarmContainer />
     </>
   );
