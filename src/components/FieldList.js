c6c9a3efeb0f6e08ac55fa6a5302496b56197971
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-06-18 18:52:32 +0100

    field list.js added

--- src/components/FieldList.js
+++ src/components/FieldList.js
@@ -1,18 +1,40 @@
-import Fields from "./Fields";
+
+import { useEffect } from "react";
+import axios from 'axios'; 
+import Farm from "../Farm/Farm";
 
 
-const FieldList = ({listOfFields}) => {
+const FieldList = ( {fieldList, setFieldList} ) => {
 
-    const fieldsComponents = listOfFields.map((fields, index) => {
-        return <Fields fields={fields} key={index}/>
-    });
 
-    return(
-        <>
-            {fieldsComponents}  
-        </>
-    )
+            useEffect( () => { axios.get('http://127.0.0.1:8080/fields')
+            .then(res => {
+        
+                const fieldList = res.data;
+            setFieldList(fieldList);
+             }).catch(err => console.log(err));
 
+  });
+
+  return (
+    <>
+        
+        <ul>
+          {fieldList.map(field =>
+              <li key={field.id}><Farm 
+                    fieldName={field.name}
+                    fieldTimeLeft={field.timeLeft}
+                    fieldSoilType={field.soilType}
+                    fieldFieldType={field.fieldType}
+                    fieldCrop={field.crop}
+                    fieldFarm={field.farm}
+                /></li>
+              )
+          }
+        </ul>
+        
+    </>
+  )
 }
 
 export default FieldList;
