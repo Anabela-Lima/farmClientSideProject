dbaa8d417132f68686e4ab3b791b6510b87126d8
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-06-19 20:43:16 +0100

    reformatting

--- src/HowToPlay/HowToPlay.js
+++ src/HowToPlay/HowToPlay.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './HowToPlay.css';
 import farmer from './htp.gif';
+import dance from './dancing.gif';
 
 const HowToPlay = () => {
     return (
     
     <div className='HowToPlayText'>
-
-    <br/><h1 className='Title'>Welcome Farmer, are you ready to create your own farm?</h1> <br/>
-
-
-{/* farmer gif */}
-   
+<img src={dance} alt="farmer gif" className='Gif1'></img>
+{/* trying to centre gif but ??? */}
+    
+    <br/><h1 className='Title'>Welcome Farmer, are you ready to create your own farm?
+    
+    </h1> <br/>
 
     <h3>First things first, decide what you want to name your farm</h3> <br/>
 
-
+    
     <h3>Start by selecting a farm name</h3> <br/>
 
     <h3>Plant some seeds</h3> <br/>
