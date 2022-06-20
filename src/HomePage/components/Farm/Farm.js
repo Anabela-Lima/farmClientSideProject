7a5e8465a97f2f04dcb14e624ea352bfbc00c7df
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-06-20 09:38:19 +0100

    push

--- src/HomePage/components/Farm/Farm.js
+++ src/HomePage/components/Farm/Farm.js
@@ -4,6 +4,7 @@ import DeleteFarm from "../DeleteFarm.js/DeleteFarm";
 import FarmHeader from "../FarmHeader/FarmHeader";
 import NewDay from "../NewDay/NewDay";
 import "./Farm.css";
+import Fields from "../Field.js/Field";
 
 const Farm = ( {farmName, farmFunds, farmDate, farmId} ) => {
 
@@ -28,7 +29,7 @@ const Farm = ( {farmName, farmFunds, farmDate, farmId} ) => {
                 </section>
 
                 <section className="fields-section">
-                    <div className="field"></div>
+                    {/* <Fields /> */}
                     <div className="field"></div>
                     <div className="field"></div>
                     <div className="field"></div>
