82f2a4ba21bbad64bbf99718fc174befa893fb19
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-06-22 14:15:18 +0100

    css  update; htp gif examples added

--- src/HowToPlay/HowToPlay.js
+++ src/HowToPlay/HowToPlay.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import './HowToPlay.css';
-import dance from './dancing-happy.gif'
+import dance from './dancing-happy.gif';
+import name from './nameyourfarm.gif';
+import fieldadd from './addField.gif';
 
 
 const HowToPlay = () => {
     return (
     
 
-    <>
-      <img src={dance} alt="dancing farm img"></img>
-    <h1>Welcome Farmer, are you ready to create your own farm?</h1>
-  
-    <h2>First things first, decide what you want to name your farm</h2>
+    <section className="HowToPlay-container">
+      {/* <img src={dance} alt="dancing farm img" id="HowToPlayimg"></img> */}
+      
+      <div className="HowToPlayText">
+              <h1> Welcome Farmers</h1>
+              <h1> are you ready to create your own farm?</h1>
+            
+              <h3>Start by selecting a farm name</h3>
+<img src={name} alt="dancing farm img" id="Namegif"></img>
+              <h3>Pick the right field for your new farm</h3>
 
-    <h3>Start by selecting a farm name</h3>
+              <img src={fieldadd} alt="dancing farm img" id="Fieldgif"></img>
 
-    <h3>Plant some seeds</h3>
+              <h3>Harvest and sell your crops to earn some money!</h3>
 
-    <h3>Harvest your crops</h3>
+          
+              <h3>Create more farms for endless fun!</h3>
 
-    <h3>Create more farms for endless fun!</h3>
+    </div>
 
-    {/* just real basic for now, add images/screenshots */}
-
-
-    </>
+    </section>
     )
 }
 
